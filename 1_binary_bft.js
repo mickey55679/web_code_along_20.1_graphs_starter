5be64c8b7ceb5433aaaf48530b01a832b3ce2bce
Michaiah Bos <michaiahbos@yahoo.com>
2024-05-08 22:25:42 -0500

    updates

--- 1_binary_bft.js
+++ 1_binary_bft.js
@@ -1,14 +1,26 @@
 class node {
   constructor(value) {
-    this.value = value
-    this.left = null
-    this.right = null
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
 
 function traverse(root) {
-  const values = []
-  const queue = []
+  const values = [];
+  const queue = [root];
+
+  while (queue.length) {
+    const currentNode = queue.shift();
+    values.push(currentNode.value);
+    if (currentNode.left) {
+      queue.push(currentNode.left);
+    }
+    if (currentNode.right) {
+      queue.push(currentNode.right);
+    }
+  }
+  return values;
 
   // your BFT code here
   // Enqueue: queue.push(value)
@@ -22,8 +34,17 @@ function traverse(root) {
 // Output: list of values in the order of a breadth-first traversal
 
 // Test cases:
-const head = new node(6)
-head.left = new node(3)
-head.right = new node(9)
+const head = new node(6);
+head.left = new node(3);
+head.right = new node(9);
 
-console.log(traverse(head)) // 6, 3, 9
+/*
+1. Initalize an empty output list [] (line 10)
+2. Initialize an empty queue --> visualize levels [] (line 11)
+3. Enqueue root inside queue [root]
+4. While values are in queue, start serving the values
+  4a. Add to ouput list, remove from queue
+  4b Enqueue children (left, right) to queue
+  5. Return output list
+ */
+console.log(traverse(head)); // 6, 3, 9
--- 2_graph_bft.js
+++ 2_graph_bft.js
@@ -1,5 +1,5 @@
 function traverse(graph, root) {
-    // Implement
+  // Implement
 }
 
 // Implement a breadth-first traversal (BFT) for a graph.
@@ -18,7 +18,18 @@ function traverse(graph, root) {
 // |  2
 // | / \
 // 1    4
-const graph = { 1: [2, 5], 2: [1, 4, 5], 3: [5], 4: [2], 5: [1, 2, 3] }
-console.log(traverse(graph, 1)) // 1, 2, 5, 4, 3
-console.log(traverse(graph, 4)) // 4, 2, 1, 5, 3
-console.log(traverse(graph, 5)) // 5, 1, 2, 3, 4
+
+/*
+1. Initalize an empty output list [] 
+2. Initialize an empty queue []
+Initialize empty visited set {}
+3. Enqueue root inside queue []
+4. While values are in queue, start serving the values
+  4a. Add to ouput list, remove from queue
+  4b Enqueue neighbors to the queue and mark as visited
+  5. Return output list
+ */
+const graph = { 1: [2, 5], 2: [1, 4, 5], 3: [5], 4: [2], 5: [1, 2, 3] };
+console.log(traverse(graph, 1)); // 1, 2, 5, 4, 3
+console.log(traverse(graph, 4)); // 4, 2, 1, 5, 3
+console.log(traverse(graph, 5)); // 5, 1, 2, 3, 4
